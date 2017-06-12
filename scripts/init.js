2d3eecfbf4f8ef9a4ac88d5dd946d54d4433421b
Ryan Delaney <rrdelaney@outlook.com>
2017-06-11 21:52:57 -0700

    Remove postinstall flow-typed script

--- scripts/init.js
+++ scripts/init.js
@@ -46,8 +46,7 @@ module.exports = function(
     start: 'react-scripts start',
     build: 'react-scripts build',
     test: 'react-scripts test --env=jsdom',
-    eject: 'react-scripts eject',
-    postinstall: 'flow-typed install && retyped --flow-typed'
+    eject: 'react-scripts eject'
   };
 
   fs.writeFileSync(
